d5734b9df5960d1b2dca65ba5a1cdae636b6edc0
khensley2592 <81331519+khensley2592@users.noreply.github.com>
2021-06-21 19:06:28 -0400

    Created React weather app in VSCode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <div className="App">
+        <div class="container">
+          <div class="wrapper">
+            <div class="row justify-content-center">
+              <div class="col-4">
+                <h1>City</h1>
+                <h2 class="date"></h2>
+                <div id="current"></div>
+                <span class="temp" id="temperature"></span>
+                <span class="units">
+                  <a href="#" id="celsius-link" class="active">
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="#" id="fahrenheit-link">
+                    °F
+                  </a>
+                </span>
+                <div>
+                  <img src="" alt="Clear" id="icon" />
+                </div>
+                <br />
+                <p>
+                  Humidity: <span id="humidity"></span>%
+                  <br />
+                  Wind: <span id="wind"></span> km/h
+                </p>
+              </div>
+              <div class="col-4">
+                <form id="search-form">
+                  <input
+                    type="search"
+                    placeholder="Enter a city"
+                    autocomplete="off"
+                    autofocus="on"
+                    i
+                    id="search-text-input"
+                    class="form-control"
+                  />
+                  <input
+                    type="submit"
+                    value="Search"
+                    class="btn btn-primary w-50"
+                  />
+                </form>
+                <br />
+                <div class="weather-forecast" id="forecast"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Wantsnewpaint/React-Weather-App"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Open-source code
         </a>
-      </header>
+        by Katie Hensley
+      </p>
     </div>
   );
 }
